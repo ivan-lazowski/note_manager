18219b72ffba8cacb88f756c2a37f46380966bfa
ivan-lazowski <ivan.lazowski@gmail.com>
2024-12-28 18:02:29 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,17 @@
+# username - имя пользователя
+# title - заголовок заметки
+# content - описание заметки
+# status - статус заметки
+# created_date - дата создания заметки в формате "день-месяц-год"
+# issue_date - дата истечения заметки (дедлайн) в формате "день-месяц-год"
+
+
+username = 'Иван'
+title =
+content =
+status =
+created_date =
+issue_date =
+
+
+print(username)
