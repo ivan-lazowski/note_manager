a672d889c95d836a74ffd13b443bdb68275a4461
ivan-lazowski <ivan.lazowski@gmail.com>
2024-12-30 20:09:08 +0400

    30.12

---
+++ 123.py
@@ -0,0 +1,18 @@
+print('Hello world')
+
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print((2 * 2 + 2)==(2 * (2 + 2)))
+
+# 4th program
+print(int(123.456 * 10 % 10))
+
+name = input('Ваше имя: ')
+age = int(input('Сколько вам лет: '))
+sum = age + 5
+print(f'Через 5 лет вам {name} исполнится: {sum}')
